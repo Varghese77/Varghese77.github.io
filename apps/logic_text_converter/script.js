2a2f8fe3918f8b0e312d06c9b697e45101c2edf4
Roy Varghese Mathew <vargs77@cs.washington.edu>
2016-10-24 15:55:54 -0700

    Adds hotkey functionality and README

--- apps/logic_text_converter/script.js
+++ apps/logic_text_converter/script.js
@@ -1,30 +1,162 @@
-// Logical symbols stored as strings
-var notSymbol = "¬";
-var andSymbol = "∧";
-var orSymbol = "∨";
-var xorSymbol = "⊕";
-var implicationSymbol = "→";
-var biconditionalSymbol = "↔";
-var equivalenceSymbol = "≡";
-var universalQuantifier = "∀";
-var existentialQuantifier = "∃";
+var textBox = new InputText();
 
-function onClick() {
-    var input = document.getElementById("textInput");
-    var rawText = input.value;
+function TextStack() {
+    this.stack = [];
     
-    rawText = rawText.replace(/{A}/g, universalQuantifier);
-    rawText = rawText.replace(/{E}/g, existentialQuantifier);
+    this.pop = function() {
+        if (this.stack.length != 0){
+            return this.stack.pop();
+        } else {
+            return "";
+        }
+    };
     
-    rawText = rawText.replace(/<->/g, biconditionalSymbol);
-    rawText = rawText.replace(/->/g, implicationSymbol);
-    rawText = rawText.replace(/:=/g, equivalenceSymbol);
-    rawText = rawText.replace(/-/g, notSymbol);
-    rawText = rawText.replace(/not /g, notSymbol);
-    rawText = rawText.replace(/not/g, notSymbol);
-    rawText = rawText.replace(/and/g, andSymbol);
-    rawText = rawText.replace(/xor/g, xorSymbol)
-    rawText = rawText.replace(/or/g, orSymbol);
+    this.push = function(s){
+        this.stack.push(s);
+    };
+    
+    this.peek = function() {
+        if (this.stack.length != 0){
+            var text = this.pop();
+            this.push(text);
+            return text;
+        } else {
+            return "";
+        }
+        
+    };
+    
+    this.size = function() {
+        return this.stack.length;
+    };
+    
+    this.clear = function() {
+        if (this.stack.length !== 0){
+            this.stack = [];
+        }
+    };
+    
+    this.size = function() {
+        return this.stack.length;
+    };
+};
 
-    input.value = rawText;
+function InputText() {
+    this.input; 
+    this.backStack = new TextStack();
+    this.fwdStack = new TextStack();
+    
+    this.add = function(s){
+        if (s !== this.backStack.peek()) {
+            this.backStack.push(s);
+            this.fwdStack.clear();
+        }
+        console.log("back: " + this.backStack.size());
+        console.log("FWD: " + this.fwdStack.size());
+    };
+    
+    this.back = function() {
+        var input = document.getElementById("textInput");
+        this.add(input.value);
+        if (this.backStack.size() !== 0){
+            this.fwdStack.push(this.backStack.pop());
+            input.value = this.backStack.peek();
+        }
+        console.log("back: " + this.backStack.size());
+        console.log("FWD: " + this.fwdStack.size());
+    };
+    
+    this.forward = function() {
+        var input = document.getElementById("textInput");
+        if (this.fwdStack.size() !== 0){
+            var fwdText = this.fwdStack.pop();
+            this.backStack.push(fwdText);
+            input.value = fwdText;
+        }
+        console.log("back: " + this.backStack.size());
+        console.log("FWD: " + this.fwdStack.size());
+    };
+    
+    this.convert = function() {
+        /* Logical symbols stored as strings
+            var notSymbol = "¬";
+            var andSymbol = "∧";
+            var orSymbol = "∨";
+            var xorSymbol = "⊕";
+            var implicationSymbol = "→";
+            var biconditionalSymbol = "↔";
+            var equivalenceSymbol = "≡";
+            var universalQuantifier = "∀";
+            var existentialQuantifier = "∃";
+        */
+        var input = document.getElementById("textInput");
+        var rawText = input.value;
+        this.add(rawText);
+
+        // Quantifiers
+        rawText = rawText.replace(/{A}/g, "∀");
+        rawText = rawText.replace(/{E}/g, "∃");
+
+        // Set Logic Symbols
+        rawText = rawText.replace(/{U}/g, "∪");
+        rawText = rawText.replace(/{u}/g, "∩");
+        rawText = rawText.replace(/{u>}/g, "⊂");
+        rawText = rawText.replace(/{ue}/g, "∈");
+        rawText = rawText.replace(/{u=}/g, "⊆");
+
+        // Propositinal Logic Symbols
+        rawText = rawText.replace(/<_>/g, "↔");
+        rawText = rawText.replace(/->/g, "→");
+        rawText = rawText.replace(/:=/g, "≡");
+        rawText = rawText.replace(/-/g, "¬");
+        rawText = rawText.replace(/not /g, "¬");
+        rawText = rawText.replace(/not/g, "¬");
+        rawText = rawText.replace(/and/g, "∧");
+        rawText = rawText.replace(/xor/g, "⊕")
+        rawText = rawText.replace(/or/g, "∨");
+
+        // Put back characters to remain non-changed. 
+        rawText = rawText.replace(/\\∨/g, "\\or");
+        rawText = rawText.replace(/\\¬/g, "\\not ");
+        rawText = rawText.replace(/\\∧/g, "\\and");
+        rawText = rawText.replace(/\\⊕/g, "\\xor");
+
+        input.value = rawText;
+        this.add(rawText);
+    };
+    
+    this.removeSlashes = function() {
+        var input = document.getElementById("textInput");
+        var rawText = input.value;
+        this.add(rawText);
+        
+        rawText = rawText.replace(/\\or/g, "or");
+        rawText = rawText.replace(/\\not /g, "not ");
+        rawText = rawText.replace(/\\and/g, "and");
+        rawText = rawText.replace(/\\xor/g, "xor");
+        input.value = rawText;
+        this.add(rawText)
+    };
+};
+
+
+document.addEventListener("keydown", function(evt) {
+    if (evt.keyCode==90 && (evt.ctrlKey)){
+        evt.preventDefault();
+        textBox.back();
+    } else if (evt.keyCode==89 && (evt.ctrlKey)) {
+        evt.preventDefault();
+        textBox.forward();
+    } else if (evt.keyCode==84 && (evt.ctrlKey)) {
+        evt.preventDefault();
+        textBox.convert();
+    }
+});
+
+function convert() { 
+    textBox.convert();
+}
+
+function removeSlashes() {
+    textBox.removeSlashes();
 }
--- apps/overwatch_counters/script.js
+++ apps/overwatch_counters/script.js
@@ -1,32 +1,38 @@
-var countersMap = [];
 
-initializeCounters();
+// Associative array that links a hero to an array of its counters
+var countersMap = initializeCounters();
 
+// Returns new Associative Array with hero counter data
 function initializeCounters() {
-countersMap["genji"] = new Array("winston", "mei", "zarya");
-countersMap["mccree"] = new Array("widowmaker", "genji", "soldier76");
-countersMap["pharah"] = new Array("mccree", "roadhog", "soldier76");
-countersMap["reaper"] = new Array("junkrat", "mccree", "pharah");
-countersMap["soldier76"] = new Array("mei", "reinhardt", "genji");
-countersMap["tracer"] = new Array("mccree", "mei", "soldier76");
-countersMap["reaper"] = new Array("mccree", "pharah", "junkrat");
-countersMap["bastion"] = new Array("genji", "tracer", "widowmaker");
-countersMap["ana"] = new Array("genji", "reaper", "tracer");
-countersMap["dva"] = new Array("mei", "symmetra", "zenyatta");
-countersMap["hanzo"] = new Array("dva", "tracer", "widowmaker");
-countersMap["junkrat"] = new Array("widowmaker", "mccree", "pharah");
-countersMap["lucio"] = new Array("mei", "mccree", "pharah");
-countersMap["mccree"] = new Array("genji", "widowmaker", "soldier76");
-countersMap["mei"] = new Array("widowmaker", "pharah", "junkrat");
-countersMap["mercy"] = new Array("mccree", "tracer", "widowmaker");
-countersMap["reinhardt"] = new Array("reaper", "winston", "pharah");
-countersMap["roadhog"] = new Array("reaper", "dva", "genji");
-countersMap["symmetra"] = new Array("junkrat", "pharah", "winston");
-countersMap["torbjorn"] = new Array("junkrat", "widowmaker", "pharah");
-countersMap["widowmaker"] = new Array("dva", "genji", "winston");
-countersMap["winston"] = new Array("reaper", "mei", "mccree");
-countersMap["zarya"] = new Array("roadhog", "pharah", "reaper");
-countersMap["zenyatta"] = new Array("hanzo", "tracer", "widowmaker");
+    var countersMap = [];
+    
+    // Adds counter data
+    countersMap["genji"] = new Array("winston", "mei", "zarya");
+    countersMap["mccree"] = new Array("widowmaker", "genji", "soldier76");
+    countersMap["pharah"] = new Array("mccree", "roadhog", "soldier76");
+    countersMap["reaper"] = new Array("junkrat", "mccree", "pharah");
+    countersMap["soldier76"] = new Array("mei", "reinhardt", "genji");
+    countersMap["tracer"] = new Array("mccree", "mei", "soldier76");
+    countersMap["reaper"] = new Array("mccree", "pharah", "junkrat");
+    countersMap["bastion"] = new Array("genji", "tracer", "widowmaker");
+    countersMap["ana"] = new Array("genji", "reaper", "tracer");
+    countersMap["dva"] = new Array("mei", "symmetra", "zenyatta");
+    countersMap["hanzo"] = new Array("dva", "tracer", "widowmaker");
+    countersMap["junkrat"] = new Array("widowmaker", "mccree", "pharah");
+    countersMap["lucio"] = new Array("mei", "mccree", "pharah");
+    countersMap["mccree"] = new Array("genji", "widowmaker", "soldier76");
+    countersMap["mei"] = new Array("widowmaker", "pharah", "junkrat");
+    countersMap["mercy"] = new Array("mccree", "tracer", "widowmaker");
+    countersMap["reinhardt"] = new Array("reaper", "winston", "pharah");
+    countersMap["roadhog"] = new Array("reaper", "dva", "genji");
+    countersMap["symmetra"] = new Array("junkrat", "pharah", "winston");
+    countersMap["torbjorn"] = new Array("junkrat", "widowmaker", "pharah");
+    countersMap["widowmaker"] = new Array("dva", "genji", "winston");
+    countersMap["winston"] = new Array("reaper", "mei", "mccree");
+    countersMap["zarya"] = new Array("roadhog", "pharah", "reaper");
+    countersMap["zenyatta"] = new Array("hanzo", "tracer", "widowmaker");
+    
+    return countersMap;
 }
 
 function heroClick(clickedId){
